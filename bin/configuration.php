0df55c773ef853f52771b93387420c71bd1a8f05
ElectricMaxxx <maximilian.berghoff@gmx.de>
2017-08-12 10:40:48 +0200

    start to write push/pull scripts

---
+++ bin/configuration.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * A list of projects with its remote names and folders;
+ *
+ * @var []
+ */
+$projects = array(
+    'seo-bundle' => array('directory' => 'src/Bundle/SeoBundle', 'repository' => 'git@github.com:symfony-cmf/seo-bundle.git'),
+);
+
+return $projects;
